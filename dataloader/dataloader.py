a548254bbc3f024f4e2951dca73e2c6ca987d2df
w20ss11 <w20ss11@gmail.com>
2022-02-15 19:50:40 +0800

    Update dataloader.py

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,17 +1,26 @@
-class FashionMNISTDataset(Dataset):
-    def __init__(self, csv_file, transform=None):
-        data = pd.read_csv(csv_file)
-        self.X = np.array(data.iloc[:, 1:]).reshape(-1, 1, 28, 28).astype(float)
-        self.Y = np.array(data.iloc[:, 0]);
-        del data;  #结束data对数据的引用,节省空间
-        self.len=len(self.X)
+from torch.utils.data import Dataset
+
+class customData(Dataset):
+    def __init__(self, img_path, txt_path, dataset = '', data_transforms=None, loader = default_loader):
+        with open(txt_path) as input_file:
+            lines = input_file.readlines()
+            self.img_name = [os.path.join(img_path, line.strip().split('\t')[0]) for line in lines]
+            self.img_label = [int(line.strip().split('\t')[-1]) for line in lines]
+        self.data_transforms = data_transforms
+        self.dataset = dataset
+        self.loader = loader
 
     def __len__(self):
-        #return len(self.X)
-        return self.len
-        
-    
-    def __getitem__(self, idx):
-        item = self.X[idx]
-        label = self.Y[idx]
-        return (item, label)
+        return len(self.img_name)
+
+    def __getitem__(self, item):
+        img_name = self.img_name[item]
+        label = self.img_label[item]
+        img = self.loader(img_name)
+
+        if self.data_transforms is not None:
+            try:
+                img = self.data_transforms[self.dataset](img)
+            except:
+                print("Cannot transform image: {}".format(img_name))
+        return img, label
