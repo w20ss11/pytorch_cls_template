48eb67470c224f66c625f75e712106b32c2209e2
w20ss11 <w20ss11@gmail.com>
2022-02-15 20:29:08 +0800

    Update dataloader.py

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,26 +1,34 @@
-from torch.utils.data import Dataset
+class CustomDataset(Dataset):
+    """Custom dataset."""
 
-class customData(Dataset):
-    def __init__(self, img_path, txt_path, dataset = '', data_transforms=None, loader = default_loader):
-        with open(txt_path) as input_file:
-            lines = input_file.readlines()
-            self.img_name = [os.path.join(img_path, line.strip().split('\t')[0]) for line in lines]
-            self.img_label = [int(line.strip().split('\t')[-1]) for line in lines]
-        self.data_transforms = data_transforms
-        self.dataset = dataset
-        self.loader = loader
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
 
     def __len__(self):
-        return len(self.img_name)
+        return len(self.landmarks_frame)
 
-    def __getitem__(self, item):
-        img_name = self.img_name[item]
-        label = self.img_label[item]
-        img = self.loader(img_name)
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-        if self.data_transforms is not None:
-            try:
-                img = self.data_transforms[self.dataset](img)
-            except:
-                print("Cannot transform image: {}".format(img_name))
-        return img, label
+        img_name = os.path.join(self.root_dir,
+                                self.landmarks_frame.iloc[idx, 0])
+        image = io.imread(img_name)
+        landmarks = self.landmarks_frame.iloc[idx, 1:]
+        landmarks = np.array([landmarks])
+        landmarks = landmarks.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'landmarks': landmarks}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
